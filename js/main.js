ef35af43d89a09d5c893a12260e66d34c0acab1a
sunshine <sweet-jh@qq.com>
2015-11-20 16:38:46 +0800

    css:add the display style

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 var bookmarks;
 var keywords;
-$.getJSON("http://sweet-jh.github.io/lesson5_js/bookmarks.json",function(data)
+$.getJSON("bookmarks.json",function(data)
 {
   bookmarks=data;
+  init();
 });
 
 //init 显示数据
@@ -12,30 +13,35 @@ function init()
   var strHtml = "";//存储数据的变量
   $jsontip.empty();//清空内容
   $.each(bookmarks,function(infoIndex,info){
-      strHtml += "title："+info["title"]+"<br>";
-      strHtml += "time："+info["created"]+"<br>";
+      strHtml += '<div class="title">'+info["title"]+'</div>';
+      strHtml += '<div class="time">'+'Created @ '+getTime(info["created"])+'</div>';
       strHtml += "<hr>"
     })
   $jsontip.html(strHtml);//显示处理后的数据
 }
 
-$(document).ready(function(){
+function getTime(unix) {
+    var d = new Date();
+    d.setTime(unix);
+    return d.getFullYear() + "-" + (d.getMonth()+1) + "-" + (d.getDate()+1);
+}
 
-    init();
+$(document).ready(function(){
 
   $("#search").bind('input propertychange',function(){
     keywords=$("#search").val();
 
     if(keywords.length>0)
     {
-      var a = new RegExp(keywords, "ig");
+      var a = new RegExp(keywords, "igm");
       var totalstr="";
       var str="";
       $.each(bookmarks,function(infoIndex,info){
-      str = "title："+info["title"];
+      str = info["title"];
       if(a.test(str))
       {
-        totalstr = totalstr + str.replace(a,("<span style='color:#F00'>" + keywords + "</span>")) + "<br>"+"<hr>";
+        str = str.replace(a,'<span id="key">$&</span>');
+        totalstr = totalstr + '<div class="title">'+str+'</div>' + '<div class="time">'+'Created @ '+getTime(info["created"])+'</div>'+"<hr>";
       }
     });
     $(".content").empty();
